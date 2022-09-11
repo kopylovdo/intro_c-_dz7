1020e96d0e4ee576ce0d3f114cd0b2e27d7abf80
Дмитрий Копылов <dkopylov@MacBook-Air-2.local>
2022-09-11 17:32:02 +0300

    Task1 init commit

---
+++ program_001_random_dbl/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными (тип double) числами
+
+double GenerateDoubleInRange(double lowerBound,double UpperBound)
+{
+    Random rnd = new Random();
+    double RandomDouble = lowerBound + rnd.NextDouble() * (UpperBound - lowerBound);
+    return RandomDouble;
+}
+
+System.Console.WriteLine("Введите количество строк");
+int rowsCount = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите количество столбцов");
+int columnsCount = Convert.ToInt32(Console.ReadLine());
+
+double[,] twoDimArray = new double[rowsCount, columnsCount];
+
+//переменная, определяющая количество знаком для округления числа double при выводе на экран
+int round_quality = 4;
+
+for (int i = 0; i < rowsCount; i++)
+{
+    for (int j = 0; j < columnsCount; j++)
+    {
+        twoDimArray[i, j] = GenerateDoubleInRange(-10,10);
+        System.Console.Write(Math.Round(twoDimArray[i, j],round_quality) + "\t\t");
+    }
+    System.Console.WriteLine();
+}
+System.Console.WriteLine();
